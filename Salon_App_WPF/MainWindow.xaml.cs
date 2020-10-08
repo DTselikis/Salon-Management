d2a6bff5ae222161354a804028d363a048211580
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2020-10-08 17:42:01 +0300

    Menu buttons
    Added the two menu buttons and a basic visual functionality such as: change color and curson on hover and change color on mouse down.

--- Salon_App_WPF/MainWindow.xaml.cs
+++ Salon_App_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,64 @@ namespace Salon_App_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private StackPanel currentBtn;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ActivateButton(object senderBtn, SolidColorBrush color)
+        {
+            if (senderBtn != null)
+            {
+                // Disable previous button before enable current
+                DisableButton();
+
+                // Get StackPanel
+                currentBtn = (StackPanel)senderBtn;
+                currentBtn.Background = new SolidColorBrush(Color.FromRgb(65, 66, 94));
+
+                // Apply color to the left of the selected button
+                Grid btnLeftBorder = (Grid)currentBtn.Children[0];
+                btnLeftBorder.Background = color;
+
+                //Change the color of the icon and the text of the button
+                IconBlock btnIcon = (IconBlock)currentBtn.Children[1];
+                btnIcon.Foreground = color;
+
+                TextBlock btnText = (TextBlock)currentBtn.Children[2];
+                btnText.Foreground = color;
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                // Reset values to default
+                currentBtn.Background = new SolidColorBrush(Color.FromRgb(48, 49, 69));
+
+                Grid btnLeftBorder = (Grid)currentBtn.Children[0];
+                btnLeftBorder.Background = new SolidColorBrush(Colors.Transparent);
+
+                IconBlock btnIcon = (IconBlock)currentBtn.Children[1];
+                btnIcon.Foreground = new SolidColorBrush(Colors.Gainsboro);
+
+                TextBlock btnText = (TextBlock)currentBtn.Children[2];
+                btnText.Foreground = new SolidColorBrush(Colors.Gainsboro);
+            }
+        }
+
+        private void customerBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ActivateButton(sender, new SolidColorBrush(Color.FromRgb(249, 46, 151)));
+        }
+
+        private void customersBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ActivateButton(sender, new SolidColorBrush(Color.FromRgb(53, 249, 26)));
         }
     }
 }
