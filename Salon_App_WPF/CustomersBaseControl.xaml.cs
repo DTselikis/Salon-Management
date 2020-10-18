07d6cc8101b5ae6956884458654d5adc3567430a
Dimitrios Tselikis <dimitriostselikis@gmail.com>
2020-10-18 16:09:43 +0300

    Implemented delete option for customerBase
    Delete option via context menu

--- Salon_App_WPF/CustomersBaseControl.xaml.cs
+++ Salon_App_WPF/CustomersBaseControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -23,7 +24,7 @@ namespace Salon_App_WPF
     public partial class CustomersBaseControl : UserControl
     {
         private string connStr = Properties.Settings.Default.DBConnStr;
-
+        private ObservableCollection<Customer> Customers { get; set; }
         public CustomersBaseControl()
         {
             InitializeComponent();
@@ -45,18 +46,98 @@ namespace Salon_App_WPF
                     return;
                 }
 
-                string query = "SELECT FirstName, LastName, Phone, Email, FirstVisit, Gender FROM dbo.Customers;";
+                string query = "SELECT * FROM dbo.Customers;";
 
-                DataTable data = new DataTable("Employee");
                 SqlCommand command = new SqlCommand(query, dbConn);
-                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
 
-                dataAdapter.Fill(data);
-                CustomersBase.ItemsSource = data.DefaultView;
+                SqlDataReader dataReader = command.ExecuteReader();
 
+                // Use ObservableCollection so everytime there is a change to the collection
+                // it will appear to the GUI as well
+                Customers = new ObservableCollection<Customer>();
+                while (dataReader.Read())
+                {
+                    int customerID;
+                    string firstName;
+                    string lastName;
+                    string phone;
+                    string email;
+                    Nullable<DateTime> dateTime = null;
+                    char gender;
+
+                    customerID = dataReader.GetInt32(0);
+                    if (dataReader[1] != System.DBNull.Value) firstName = dataReader.GetString(1); else firstName = String.Empty;
+                    if (dataReader[2] != System.DBNull.Value) lastName = dataReader.GetString(2); else lastName = String.Empty;
+                    if (dataReader[3] != System.DBNull.Value) phone = dataReader.GetString(3); else phone = String.Empty;
+                    if (dataReader[4] != System.DBNull.Value) email = dataReader.GetString(4); else email = String.Empty;
+                    if (dataReader[5] != System.DBNull.Value) dateTime = dataReader.GetDateTime(5); else dateTime = null;
+                    if (dataReader[6] != System.DBNull.Value) gender = Char.Parse(dataReader.GetString(6).Substring(0, 1)); else gender = '\0';
+
+                    Customers.Add(new Customer(customerID, firstName, lastName, phone, email, dateTime, gender));
+                }
+
+
+                CustomersBase.ItemsSource = Customers;
+
+                dataReader.Close();
+                dbConn.Close();
+            }
+        }
+
+        private void DeleteRecord(Customer customer)
+        {
+            using (SqlConnection dbConn = new SqlConnection(connStr))
+            {
+
+                try
+                {
+                    dbConn.Open();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Παρουσιάστηκε πρόβλημα κατά στη σύνδεση. Παρακαλούμε επικοινωνήστε με το τεχνικό τμήμα.",
+                        "ΠροέκυψεΠπρόβλημα",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error,
+                        MessageBoxResult.OK);
+
+                    return;
+                }
+
+                string query = "DELETE dbo.Customers WHERE CustomerID = @ID";
+
+                SqlCommand command = new SqlCommand(query, dbConn);
+
+                command.Parameters.Add("@ID", System.Data.SqlDbType.Int);
+                command.Parameters["@ID"].Value = customer.CustomerID;
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                dataAdapter.DeleteCommand = command;
+                dataAdapter.DeleteCommand.ExecuteNonQuery();
+
+                command.Dispose();
                 dataAdapter.Dispose();
                 dbConn.Close();
             }
+
+            MessageBox.Show(
+                "Ο πελάτης διαγράφτηκε.",
+                "Επιτυχία",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information,
+                MessageBoxResult.OK);
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        { 
+            DeleteRecord((Customer)CustomersBase.SelectedItems[0]);
+            Customers.RemoveAt(CustomersBase.SelectedIndex);
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Customers.Clear();
         }
     }
 }
